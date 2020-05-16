82d694ecd545a3afcb5bf9c8e354c7e149765ac1
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-16 01:03:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  !!text.scan(/^[A-Z]{1}[.?!]$/g)
+  !!text.scan(/^[A-Z][.?!]$/g)
 end
 
 def valid_phone_number?(phone)
