a6562fa9842822d3418d64cca5bfcc9f376d60d7
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-16 01:34:07 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/^[aeiou]/i) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  array = text.scan(/^[A-Z]|\W$/)
+  if array[0] == array[0].upcase && array[1] == "." || array[1] == "?" || array[1] == "!"
+    true 
+  elsif array[0] == array[0].downcase
+    false 
+  else 
+    false
+  end 
 end
 
 def valid_phone_number?(phone)
-
+  valid = phone.scan(/([0-9])/)
+  if valid.length == 10 
+    true 
+  else 
+    false 
+  end 
 end
